3f9da1c4bf869e9aee119e1c8134075ea4f9fb82
JOBIN-SER <103894324+JOBIN-SER@users.noreply.github.com>
2022-04-23 12:26:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,17 +22,17 @@ global.botname = "𝙱𝙾𝚃 𝚅.99" //namabot kalian
 global.ownername= "𝐉𝐎𝐁𝐈𝐍-𝐒𝐄𝐑" //nama kalian
 global.myweb ="https://chat.whatsapp.com/D9A4Q9dhtQ03pTfenn2Jl5" //bebas asal jan hapus
 global.youtube = "https://youtube.com/channel/UCuJFcmDliVZqbzJVtjflvdw" //bebas asal jan hapus
-global.github = "https://Nichuvdr.github.io/" //bebas
-global.email = "pnichu786@gmail.com" //bebas
+global.github = "https://github.com/JOBIN-SER" //bebas
+global.email = "tmmass000@gmail.com" //bebas
 global.region = "India" //bebas
-global.ownernomer = "919207980284" // nomor wa kalian
-global.ownernomerr = "+919207980284" //nmr wa kalian
+global.ownernomer = "917593010965" // nomor wa kalian
+global.ownernomerr = "+917736240975" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.donasi = "https://i.imgur.com/SrEtYu0.jpeg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["919207980284","917994107442","919207980284"] //ganti agar fitur owner bisa di gunakan
+global.owner = ["917593010965"] //ganti agar fitur owner bisa di gunakan
 global.packname = '© 𝙱𝙾𝚃 𝚅.1' //sticker wm ubah
-global.author = '𝐵𝑦 𝑁𝑖𝑐ℎ𝑢 𝑣𝑑𝑟' //sticker wm ganti nama kalian
+global.author = 'ʙʏ ᴊᴏʙɪɴ-ꜱᴇʀ' //sticker wm ganti nama kalian
 global.sessionName = '𝐿𝑖𝑣𝑒 𝑛𝑜𝑤'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
